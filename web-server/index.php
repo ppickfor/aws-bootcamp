d78775acc275326bb68bd9cd58e2f306da7898aa
Brett Gillett <brett@ei2.ca>
2016-08-20 11:39:08 -0400

    modified directory structure
    	renamed:    bootcamp-presentation.pptx -> mississauga/bootcamp-presentation.pptx
    	modified:   ../web-server/index.php

--- web-server/index.php
+++ web-server/index.php
@@ -4,18 +4,31 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <title>AWS Bootcamp</title>
   </head>
-  </body>
+  <body>
     <div class="container">
       <div class="row">
         <div class="col-xs-12 text-center">
-          <h1>AWS Bootcamp 2016</h1>
-          <h2>Dallas, Texas</h2>
+          <h1>AWS Bootcamp 2016 Tour</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 text-center">
+          <ul class="breadcrumb">
+            <li>Mississauga</li>
+            <li>Dallas</li>
+            <li>Chicago</li>
+            <li>Atlanta</li>
+            <li>Toronto</li>
+            <li>Vancouver</li>
+          </ul>
         </div>
       </div>
       <div class="row">
         <div class="col-xs-12">
           <img class="img-responseive center-block" src="luigi.jpg">
         </div>
+      </div>
+      <div>
         <div class="col-xs-12 text-center">
           EC2 Private IP Address: <?php echo $_SERVER['SERVER_ADDR'];?>
         </div>
